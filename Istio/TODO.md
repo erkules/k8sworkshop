346a9cdeafc32b830f6b604077be9d64c1ab3739
erkan <you@example.com>
2020-05-06 20:27:38 +0200

    benamsung

---
+++ Istio/TODO.md
@@ -0,0 +1,3 @@
+Dienst A ruft in einer Schleife dienst B oder C auf
+Kann man in kali sehen
+in jaeger sehen
--- Knative/Install/install.sh
+++ Knative/Install/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-kubectl apply --selector knative.dev/crd-install=true -f knative-alles.yaml
-sleep 5
-kubectl apply --selector knative.dev/crd-install=true -f knative-alles.yaml
+kubectl apply --wait=true --selector  knative.dev/crd-install=true -f knative-alles.yaml
+kubectl apply --wait=true --selector  knative.dev/crd-install=true -f knative-alles.yaml
 kubectl apply -f knative-alles.yaml  
