58e08de37a0b738a7115c218efe71f37099f2678
erkan <you@example.com>
2023-08-17 05:33:04 +0200

    pfft

--- Storage/MySQL-Example/deploy.sh
+++ Storage/MySQL-Example/deploy.sh
@@ -1,9 +1,9 @@
-kubectl apply -f mysqld-pvc.yml
-kubectl apply -f mysqld-deploy.yml
+kubectl apply -f mysqld-pvc.yaml
+kubectl apply -f mysqld-deploy.yaml
 kubectl wait --for=condition=ready deploy mysqld
 kubectl apply -f mysqld-svc.yaml
-kubectl apply -f mysql-client-deploy.yml
-kubectl apply -f mysql-client-ing.yml
-kubectl apply -f mysql-client-svc.yml
+kubectl apply -f mysql-client-deploy.yaml
+kubectl apply -f mysql-client-ing.yaml
+kubectl apply -f mysql-client-svc.yaml
 kubectl apply -f mysqld-load-data.yaml
 
